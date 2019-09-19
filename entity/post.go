a343ce3593e3437144645b6f3ec8a390553728c7
青木 克臣 <aokikatsuomi@LAB-N1644.local>
2019-09-19 20:46:17 +0900

    fixup! first commit

--- entity/post.go
+++ entity/post.go
@@ -2,8 +2,9 @@ package entity
 
 // Post is post models property
 type Post struct {
+	gorm.Model
 	ID        uint   `json:"id"`
 	Content   string `json:"content"`
-	UserId    uint   `json:"user_id"`
+	UserRefer uint
 }
 //git
--- entity/user.go
+++ entity/user.go
@@ -2,6 +2,8 @@ package entity
 
 // User is user models property
 type User struct {
+	gorm.Model
+	Posts []Post
 	ID        uint   `json:"id"`
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
